b088b5fd9b70840776c18d44456573facced5354
Paulo Gustavo <pgustavodlima@gmail.com>
2025-02-26 23:05:32 -0300

    autenticacao

--- app/Views/auth/forgot-password.php
+++ app/Views/auth/forgot-password.php
@@ -1,5 +1,6 @@
 <?php
 $title = 'Recuperar Senha - ' . APP_NAME;
+$flash_messages = \App\Core\View::getFlashMessages();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -10,12 +11,12 @@ $title = 'Recuperar Senha - ' . APP_NAME;
     <title><?= $title ?></title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= asset('vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= asset('css/sb-admin-2.min.css') ?>" rel="stylesheet">
-    <link href="<?= asset('css/style.css') ?>" rel="stylesheet">
+    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-primary">
@@ -33,15 +34,17 @@ $title = 'Recuperar Senha - ' . APP_NAME;
                                         <p class="mb-4">Digite seu endereço de e-mail abaixo e enviaremos um link para redefinir sua senha.</p>
                                     </div>
 
-                                    <?php foreach (\App\Core\View::getFlashMessages() as $flash): ?>
-                                        <div class="alert alert-<?= $flash['type'] ?> alert-dismissible fade show mb-4" role="alert">
-                                            <i class="fas fa-exclamation-circle mr-2"></i>
-                                            <?= $flash['message'] ?>
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                    <?php endforeach; ?>
+                                    <?php if (isset($flash_messages)): ?>
+                                        <?php foreach ($flash_messages as $flash): ?>
+                                            <div class="alert alert-<?= $flash['type'] ?> alert-dismissible fade show mb-4" role="alert">
+                                                <i class="fas fa-exclamation-circle mr-2"></i>
+                                                <?= $flash['message'] ?>
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
 
                                     <form class="user" action="/forgot-password" method="POST">
                                         <div class="form-group">
@@ -66,13 +69,13 @@ $title = 'Recuperar Senha - ' . APP_NAME;
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= asset('vendor/jquery/jquery.min.js') ?>"></script>
-    <script src="<?= asset('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= asset('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+    <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?= asset('js/sb-admin-2.min.js') ?>"></script>
+    <script src="/js/sb-admin-2.min.js"></script>
 </body>
 </html>
